1f6c0f8c3ab8f5d24ae33a592d6b0d7eb99508e3
Muzafar Ahmad <ahmadmuzafar580@gmail.com>
2026-07-11 12:02:53 +0530

    Describe what you changed

--- 1.c
+++ 1.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
-
-int main()
+int main ()
 {
-    int square;
-    int area;
-    int perimeter;
-
-    printf("Enter the side of the square: ");
-    scanf("%d", &square);
-
-    area = square * square;
-    perimeter = 4 * square;
-
-    printf("Area of the square is %d\n", area);
-    printf("Perimeter of the square is %d\n", perimeter);
-
-    return 0;
+    
 }
