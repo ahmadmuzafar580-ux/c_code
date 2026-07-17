ac6fe4678865212f8f2906a76c9b554f9d1d8d91
Muzafar Ahmad <ahmadmuzafar580@gmail.com>
2026-07-17 08:32:43 +0530

    Updated project files

--- 1.c
+++ 1.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
-int main ()
+
+int main()
 {
-    
+    int a, b, c;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("Enter third number: ");
+    scanf("%d", &c);
+
+    if (a >= b && a >= c)
+    {
+        printf("%d is the biggest number.", a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("%d is the biggest number.", b);
+    }
+    else
+    {
+        printf("%d is the biggest number.", c);
+    }
+
+    return 0;
 }
